294ddb4ac1e16fbd8d2cb6675b36ee43633737c6
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-07-12 14:11:19 +0200

    Updated Pet code
    Added Validation  expression using regular expresions

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+  let input = document.getElementById("petInput").value;
+  let defaultname = "pet_";
+  // Validation logic goes here
+  let finalResult = "pet_";
+  // Placeholder for validation result
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+  let validexpresion = /^pet_[a-z A-Z 0-9]+$/; // Ive learned that if we break this  code down we have this we call it regular expressions.  '/^' = this means we start looking for the following word  which is pet_ after that  we have an array that stipulates  The + means it has to have whatever is in the array whilst the $ means the end of the search
 
-            document.getElementById('result').innerText = result;
+  if (validexpresion.test(input)) {
+    result = "Valid Syntex";
+  } else {
+    result = "Invalid syntex";
+  }
+
+  //TEst allows us to check if a permet
+
+  // TODO: Write your validation logic here
+  // Check if input starts with 'pet_' and followed by alphanumeric characters
+
+  document.getElementById("result").innerText = result;
 }
-
-
